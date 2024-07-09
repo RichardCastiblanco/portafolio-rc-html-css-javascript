e31a9443e859a4c4a185a9601b2d54c03d6b1fa4
richardcastiblanco4 <richardcastiblanco4@gmail.com>
2024-07-09 18:42:20 +0000

    parte-3

--- index.js
+++ index.js
@@ -7,3 +7,19 @@ document.getElementById('input').addEventListener('change', () => {
       document.body.classList.remove('dark');
     }
 });
+
+//funcion contacto
+function click(){
+  Swal.fire({
+    title: "Sweet!",
+    text: "Modal with a custom image.",
+    imageUrl: "https://unsplash.it/400/200",
+    imageWidth: 400,
+    imageHeight: 200,
+    imageAlt: "Custom image"
+  });
+}
+
+// JavaScript para actualizar automáticamente el año en el copyright
+ const currentYear = new Date().getFullYear();
+ document.getElementById('copyright').innerHTML = `&copy; ${currentYear} richard castiblanco. Todos los derechos reservados.`;
